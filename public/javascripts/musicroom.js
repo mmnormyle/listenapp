0cf6d713f33fde25f151cb1370ca7918e3870f57
Matthew Normyle <mmnormyle@gmail.com>
2016-06-21 22:21:22 -0700

    Added loading screen

--- public/javascripts/musicroom.js
+++ public/javascripts/musicroom.js
@@ -12,6 +12,35 @@ $(document).ready(function(){
 		roomName = pathname.replace('\/rooms/', '');
 	}
 
+	mGlobals.ui.div_loading = $("#div_loading");
+	mGlobals.ui.div_everything = $("#div_everything");
+
+
+	var opts = {
+	  lines: 13 // The number of lines to draw
+	, length: 28 // The length of each line
+	, width: 14 // The line thickness
+	, radius: 42 // The radius of the inner circle
+	, scale: 1 // Scales overall size of the spinner
+	, corners: 1 // Corner roundness (0..1)
+	, color: '#000' // #rgb or #rrggbb or array of colors
+	, opacity: 0.25 // Opacity of the lines
+	, rotate: 0 // The rotation offset
+	, direction: 1 // 1: clockwise, -1: counterclockwise
+	, speed: 1 // Rounds per second
+	, trail: 60 // Afterglow percentage
+	, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+	, zIndex: 2e9 // The z-index (defaults to 2000000000)
+	, className: 'spinner' // The CSS class to assign to the spinner
+	, top: '50%' // Top position relative to parent
+	, left: '50%' // Left position relative to parent
+	, shadow: false // Whether to render a shadow
+	, hwaccel: false // Whether to use hardware acceleration
+	, position: 'absolute' // Element positioning
+	}
+	var target = document.getElementById('div_loading');
+	mGlobals.ui.spinner = new Spinner(opts).spin(target);
+
 	mGlobals.url_room = roomName;
 
 	mGlobals.ui.input_search = $("#input_search");
@@ -111,7 +140,9 @@ function searchEnterPressed(input_search) {
 }
 
 function sessionReadyUI(roomName) {
-	$(".div_in_room").fadeIn(700);
+	mGlobals.ui.div_loading.hide();
+	mGlobals.ui.spinner.stop();
+	mGlobals.ui.div_everything.animate({opacity: 1}, 'fast');
 	setTimeout(function() {
 		$("#p_invite_friends").fadeOut(700);
 	}, 10000);
